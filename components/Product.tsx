033fbaafc0de4c210b381549d5c6462f7339ba40
TimWolfskin <timwolfskin013@gmail.com>
2023-01-10 09:57:35 -0800

    displays products

---
+++ components/Product.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    product: Product
+}
+
+function Product({ product }: Props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Product
