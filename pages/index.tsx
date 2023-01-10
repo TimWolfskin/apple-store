92f7a9bade8d9277bdf09c75f5b219a2aedb62ee
TimWolfskin <timwolfskin013@gmail.com>
2023-01-09 23:26:21 -0800

    added landing component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Landing from '../components/Landing'
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <main className='relative h-[-200vh] bg-[#E7ECEE]'>
+        <Landing />
+      </main>
     </div>
   )
 }
